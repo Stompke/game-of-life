b1654687d9e388d7b6c5a061190a02febd869f2a
Shawn Tompke <shawntompke@Shawns-MacBook-Pro.local>
2020-05-26 13:46:58 -0700

    grid is created and displaying 1 or 0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let dimensions = 10
+  const [ grid, setGrid ] = useState(() => {
+    let rows = []
+    for(let i = 0; i < dimensions; i ++) {
+      rows.push(Array.from(Array(dimensions), () => 1))
+    }
+    return rows}
+    )
+
+let topLeft = null
+let topMid = null
+let topRight = null
+let midLeft = null
+let midRight = null
+let bottomLeft = null
+let bottomMid = null
+let bottomRight = null
+
+// console.log(grid)
+for(var i=0; i < grid.length; i++){
+  for(var j=0; j < grid[i].length; j++){
+    if (i !== 0 && i !== grid.length-1 && j !== 0 && j !== grid[i].length-1) {
+      topLeft = grid[i-1][j-1]
+      topMid = grid[i-1][j]
+      topRight = grid[i-1][j+1]
+      midLeft = grid[i][j-1]
+      midRight = grid[i][j+1]
+      bottomLeft = grid[i+1][j-1]
+      bottomMid = grid[i+1][j]
+      bottomRight = grid[i+1][j+1]
+      console.log(`live!!! at: ${j} + ${i}`)
+    }
+  }
+}
+
+console.log(topLeft)
+console.log(topMid)
+console.log(topRight)
+console.log(midLeft)
+console.log(midRight)
+console.log(bottomLeft)
+console.log(bottomMid)
+console.log(bottomRight)
+console.log((Array.from(Array(15))))
+console.log(Array(22))
+
+// let dimensions = 10
+// let rows = []
+// for(let i = 0; i < dimensions; i ++) {
+//   rows.push(Array.from(Array(dimensions), () => 0))
+// }
+// console.log(rows)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {grid.map(row => {
+        return(
+          <>
+          {row.map(box => `[${box}]`)}
+          <br/>
+          </>
+        )
+        
+      })}
     </div>
   );
 }
